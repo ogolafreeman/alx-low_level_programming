c4fc2be9c12bf38d7bbc9c81f19707c95ecd8262
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-03-16 11:33:32 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, nnnut the outcome is a paeace of art");
+	return (0);
+}
