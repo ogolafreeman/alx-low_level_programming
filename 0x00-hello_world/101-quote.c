bc36af6bb9c387ec1c25c39aa794fd847daead7a
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-03-16 13:06:14 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,7 @@
  */
 int main(void)
 {
-	write(2,"and that piece of artis useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2,
+	"and that piece of artis useful\" - Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
