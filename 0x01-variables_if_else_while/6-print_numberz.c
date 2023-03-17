dc0bba11d13d19985485a77a21ed3774e54b0f90
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-03-17 15:53:06 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x = 48;
+
+	while (x <= 58)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
