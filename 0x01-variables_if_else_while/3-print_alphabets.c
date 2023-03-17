339d4a962aa6564a4b1fac0de6c14c01c1ceadb6
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-03-17 14:13:09 -0700

    capital

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char xy = 'a';
+	char ab = 'A';
+
+	while (xy <= 'z' && ab = 'Z')
+	{
+		putchar(xy);
+		putchar(ab);
+		xy++;
+		ab++;
+	}
+	putchar('\n');
+	return (0);
+}
