278a935213072667c77b7e8202301f33696c298a
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-03-16 23:13:15 -0700

    101 QUOTE

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 #include<unistd.h>
-
 /**
- * main - Entry point
+ * Main - Entry point
  *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
 	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-	return (1);
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (0);
 }
