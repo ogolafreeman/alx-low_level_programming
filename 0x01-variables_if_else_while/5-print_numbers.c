c60310829ba11f6e9fc2431c184b3dba6616c444
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-03-17 15:21:37 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdion.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 10)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
