a3d18757ca3c1d68f57e52a1e226250582a244b6
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-03-17 16:41:11 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	char xy;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(n);
+	}
+	for (xy = 'a'; xy <= 'f'; xy++)
+	{
+		putchar(xy);
+	}
+	putchar('\n');
+	return (0);
+}
