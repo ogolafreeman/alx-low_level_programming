49520a01de315873aa462d68b1db53f460e17017
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-03-17 13:31:40 -0700

    alphabetical

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+int main()
+{
+	char xy = ' a ';
+
+	while (xy <= 'z')
+	{
+		putchar(xy);
+		putchar("\n)");
+		xy++,
+	}
+	return (0);
+}
