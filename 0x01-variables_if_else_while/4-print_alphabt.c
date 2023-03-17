b6f1382ea16874f59379d0bae82255e646284293
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-03-17 15:13:49 -0700

    print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char xy = 'a';
+
+	while (xy <= 'z')
+	{
+		if (xy != 'e' && xy != 'q')
+		{
+			putchar(xy);
+		}
+		xy++;
+	}
+	putchar('\n');
+	return (0);
+}
