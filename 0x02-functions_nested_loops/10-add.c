fb95c7cc61830516766b0e6e7c47eb20baaaddd0
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-03-21 15:01:36 -0700

    add and b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,7 +3,7 @@
  * add - adds two integers and returns the results
  * @a: int to be assed to b
  * @b: int to be added to a
- * Retur: sum of a and b
+ * Return: sum of a and b
  */
 int add(int a, int b)
 {
