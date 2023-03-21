81581e7d16eec7ed065102bfecfcb88632a331f9
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-03-21 11:24:13 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int r;
+
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o;);
+	_putchar(r + '0');
+	r = _is;ower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
