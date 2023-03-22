d3768d0a0525c93d696a6848af6387cf9fec4db5
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-03-22 11:50:44 -0700

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,41 +8,33 @@ void print_times_table(int n)
 {
 	int x, y, z;
 
-	if (n >= 0 && n <= 15)
+	if (n >= 0 && n <== 15)
 	{
 		for (x = 0; x <= n; x++)
 		{
-			for (y = 0; y <= n; y++)
+			_putchar('0');
+
+			for (y = 1; y <= n; y++)
 			{
-				z = y * x;
-				if (y == 0)
+				_putchar(',');
+				_putchar(' ');
+
+				z = x * y;
+				if (z <= 98 && z <= 9)
 				{
-					_putchar(z + '0');
+					_putchar(' ');
+					_putchar(' ');
 				}
-				else if (z < 10 && y != 0)
+				if (z >= 100)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(z + '0');
+					_putchar((z / 100) + 'o');
+					_putchar(((z / 10)) % 10 + '0');
 				}
-				else if (z >= 10 && z < 100)
+				else if (z <= 99 && z>= 10)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
 					_putchar((z / 10) + '0');
-					_putchar((z % 10) + '0');
 				}
-				else if (z >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((z / 100) + '0');
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
-				}
+				_putchar((z % 10) + '0');
 			}
 			_putchar('\n');
 		}
