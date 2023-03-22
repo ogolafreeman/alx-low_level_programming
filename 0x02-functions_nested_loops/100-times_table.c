526846be3e6963addfd01aaa4f1dbfa8b0c6eb33
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-03-22 12:09:01 -0700

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,7 +27,7 @@ void print_times_table(int n)
 					_putchar(' ');
 				if (z >= 100)
 				{
-					_putchar((z / 100) + 'o');
+					_putchar((z / 100) + '0');
 					_putchar(((z / 10)) % 10 + '0');
 				}
 				else if (z <= 99 && z >= 10)
