923244aa381dc53a41062e1e8fa356960f9c7f38
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-03-22 01:54:37 -0700

    sum of even no

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - finds and prints the sum of the even values followed
+ * by a new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x;
+	unsigned long y, z, next, sum;
+
+	y = 1;
+	z = 2;
+	sum = 0;
+
+	while (x <= 33)
+	{
+		if (y < 4000000 && (y % 2) ==0)
+		{
+			sum = sum + y;
+		}
+		next = y + z;
+		y = z;
+		z = next;
+		x++;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
