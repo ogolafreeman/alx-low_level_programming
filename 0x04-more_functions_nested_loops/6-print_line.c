5c86aca0c312e7d6ce2303b6383f5c5c9219917b
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-03-23 15:16:43 -0700

    stratigt line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * print_line - draws a straight line in the terminal
- * @n; number of times the character _ should be printed
+ * @n: number of times the character _ should be printed
  */
-void print_line(int n);
+void print_line(int n)
 {
 	if (n <= 0)
 	{
