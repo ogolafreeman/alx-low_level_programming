e91cbfab657f91695886255c415bc4e30c487f18
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-03-23 14:56:25 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers
+ */
+
+void more_numbers(void)
+{
+	int x, b;
+
+	for (x = 1; x<= 10; x++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b >= 10)
+				_putchar('1');
+			_putchar(b % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
