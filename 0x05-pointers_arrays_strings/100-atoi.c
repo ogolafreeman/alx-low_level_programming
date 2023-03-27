4f321680cec99d6ee51829a5a0444247666187d4
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-03-27 14:46:54 -0700

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * _atoi - converst a string to an int
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int a, b, c, e, f, g;
+
+	a = 0;
+	b = 0;
+	c = 0;
+	e = 0;
+	f = 0;
+	g = 0;
+
+	while (s[e] != '\0')
+		e++;
+	while (a < e && f == 0)
+	{
+		if (s[a] == '-')
+			++b;
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			g = s[a] - '0';
+			if (b % 2)
+				g = g;
+			c = c * 10 + g;
+			f = 1;
+			if (s[a + 1] < '0' || s[a + 1] > '9')
+				break;
+			f = 0;
+		}
+		a++;
+	}
+	if (f == 0)
+		return (0);
+	return (n);
+}
