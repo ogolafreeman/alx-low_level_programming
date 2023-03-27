bb6c62c733e59ae68d77231c8d863b859da2055e
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-03-27 12:52:23 -0700

    puts 2

---
+++ 0x05-pointers_arrays_strings/puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts2 - functions should print only one char of two
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int a = 0;
+	int b = 0;
+	char *c = str;
+	int d;
+
+	while (*c != '\0')
+	{
+		c++;
+		a++;
+	}
+	e = a - 1;
+	for (d = 0; d <= e; d++)
+	{
+		if (d % 2 == 0)
+		{
+			_putchar(str[0]);
+		}
+	}
+	_putchar('\n');
+}
