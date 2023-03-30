f97f71c5081a4850e529f837e7d5dea433359e60
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-03-30 14:54:55 -0700

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,28 +1,31 @@
 #include "main.h"
+
 /**
  * rev_string - reverse array
  * @n: integer params
  * Return: 0
  */
+
 void rev_string(char *n)
 {
-	int x = 0;
-	int y = 0;
-	char alx;
+	int i = 0;
+	int j = 0;
+	char temp;
 
-	while (*(n + x) != '\0')
+	while (*(n + i) != '\0')
 	{
-		x++;
+		i++;
 	}
-	x--;
+	i--;
 
-	for (y = 0; y < x; y++, x--)
+	for (j = 0; j < i; j++, i--)
 	{
-		alx = *(n = y);
-		*(n + y) = *(n + x);
-		*(n + x) = alx;
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
 	}
 }
+
 /**
  * infinite_add - add 2 numbers together
  * @n1: text representation of 1st number to add
@@ -31,29 +34,30 @@ void rev_string(char *n)
  * @size_r: buffer size
  * Return: pointer to calling function
  */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int overflow = 0, x = 0, y = 0, digits = 0;
+	int overflow = 0, i = 0, j = 0, digits = 0;
 	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	while (*(n1 + x) != '\0')
-		x++;
-	while (*(n2 + y) != '\0')
-		y++;
-	x--;
-	y--;
-	if (y >= size_r || x >= size_r)
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
 		return (0);
-	while (y >= 0 || x >= 0 || overflow == 1)
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		if (x < 0)
+		if (i < 0)
 			val1 = 0;
 		else
-			val1 = *(n1 + x) - '0';
-		if (y < 0)
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
 			val2 = 0;
 		else
-			val2 = *(n2 + y) - '0';
+			val2 = *(n2 + j) - '0';
 		temp_tot = val1 + val2 + overflow;
 		if (temp_tot >= 10)
 			overflow = 1;
@@ -63,8 +67,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			return (0);
 		*(r + digits) = (temp_tot % 10) + '0';
 		digits++;
-		y--;
-		x--;
+		j--;
+		i--;
 	}
 	if (digits == size_r)
 		return (0);
