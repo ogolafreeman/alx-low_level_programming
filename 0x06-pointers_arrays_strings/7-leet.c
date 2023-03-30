9a9597f3a7e66991b18ec40f4ec9ff3c6b34a047
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-03-30 12:30:00 -0700

    7-leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
+ */
+char *leet(char *n)
+{
+	int x, y;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (x = 0; n[x] != '\0'; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (n[x] == s1[y])
+			{
+				n[x] = s2[y];
+			}
+		}
+	}
+	return (n);
+}
