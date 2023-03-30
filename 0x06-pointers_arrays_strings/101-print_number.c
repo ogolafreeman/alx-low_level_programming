f9234569e98a48195678258ec17d2a537cf8b26b
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-03-30 12:47:42 -0700

    101-print numbers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_number - point numbers chars
+ * @n: integer params'Return: 0
+ */
+void print_number(int n)
+{
+	unsigned int x;
+
+	x = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -n;
+	}
+	if (x / 10 != 0)
+	{
+		print_number(x / 10)
+	}
+	_putchar((x % 10) + '0')
+}
