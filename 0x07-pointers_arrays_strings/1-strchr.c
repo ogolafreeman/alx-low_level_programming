8c257ac2991c1ec717a4efd7b4fb7b6a6ece11c8
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-04-03 11:29:11 -0700

    a function that loctaes a character in a string

---
+++ 0x07-pointers_arrays_strings/1-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strchr - A function that locates a character in a string
+ * @s: input value
+ * @c: input value
+ *
+ * Return: Always s or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (NULL);
+}
