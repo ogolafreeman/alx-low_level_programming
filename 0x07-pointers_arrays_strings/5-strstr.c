ad34cf1c9f7ecb7ead36e972815a404bb1db72aa
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-04-03 13:20:52 -0700

    locating substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: input value
+ * @needle: input value
+ *
+ * Return: Always 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int x = strlen(needle);
+	int y = strlen(haystack);
+
+	for (int z = 0; z < y; z++)
+	{
+		if (haystack[z] == needle[0])
+		{
+			int a;
+
+			for (a = 1; a < x; a++)
+			{
+				if (haystack[z + a] != needle[a])
+				{
+					break;
+				}
+			}
+			if (a == x)
+			{
+				return (&haystac[z]);
+			}
+		}
+	}
+	return (0);
+}
