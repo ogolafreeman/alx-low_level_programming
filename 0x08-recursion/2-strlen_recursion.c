9b52d2d7c153a077d5452708377524d69ee64c5b
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-04-05 14:51:13 -0700

    string length recursion; ,

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,7 +3,7 @@
  * _strlen_recursion - Returns the length of a string
  * @s: The string to be measured
  *
- * Returm: Length of the string
+ * Return: Length of the string
  */
 int _strlen_recursion(char *s)
 {
