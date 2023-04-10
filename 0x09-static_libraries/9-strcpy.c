e9dfebddcff97d4709082538934bf635a7a13777
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-04-10 14:26:54 -0700

    0x09 - static libraries

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * char *_strcpy - a function that copies a string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a =  0;
+	int b = 0;
+
+	while (*(src + a) != '\0')
+	{
+		a++;
+	}
+	for (; b < a; b++)
+	{
+		dest[b] = src[b];
+	}
+	dest[a] = '\0';
+	return (dest);
+}
