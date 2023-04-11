72b262b489dd4783fff4338a4b14a2675eedd899
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-04-11 16:20:03 -0700

    create

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 
 	str = malloc(sizeof(char) * size);
 	if (size == 0 || str == NULL)
-		RETURN (NULL);
+		return (NULL);
 	for (x = 0; x < size; x++)
 		str[x] = c;
 	return (str);
