2eeb28841d09d454800535cc6fa5d9f484136c89
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-04-11 16:54:54 -0700

    contact

---
+++ 0x0B-malloc_free/2-str_contact.c
@@ -0,0 +1,39 @@
+#include "maiin.h"
+#include <stdlib.h>
+/**
+ * str_concat - get ends of iput and add together for size
+ * @s1: input
+ * @s2: input
+ * Return: concat
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	int a, b;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	a = b = 0;
+	while (s1[a] != '\0')
+		a++;
+	while (s2[b] != '\0')
+		b++;
+	concat = malloc(sizeof(char) * (a + b + 1));
+	if (concat == NULL)
+		return (NULL);
+	a = b = 0;
+	while (s1[a] != '\0')
+	{
+		concat[a] = s1[a];
+		a++;
+	}
+	while (s2[b] != '\0')
+	{
+		concat[a] = s2[b];
+		a++, b++;
+	}
+	concat = '\0';
+return (concat);
+}
