b6acbf007f41618f21356fbdfe468ec70c2635e6
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-04-11 17:45:02 -0700

    m

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * argstostr - main entry
+ * @ac: int input
+ * @av: double printer array
+ * Return: 0
+ */
+char *argstostr(int ac, char **av)
+{
+	int x, y, z = 0, a = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (x = 0; x < ac; x++)
+	{
+		for (y = 0; av[x][y]; y++)
+			x++;
+	}
+	a += ac;
+	str = malloc(sizeof(char) * a + 1);
+	if (str == NULL)
+		return (NULL);
+	for (x = 0; x < ac; x++)
+	{
+		for (y = 0; av[a][b]; y++)
+		{
+			str[z] = av[a][b];
+			z++;
+		}
+		if (str[z] == '\0')
+		{
+			str[z++] = '\n';
+		}
+	}
+	return (str);
+}
