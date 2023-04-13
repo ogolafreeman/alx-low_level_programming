3fb79dfb9e6301d0c52c74060952f729bdbde435
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-04-13 14:12:43 -0700

    string nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		x[m++] = s2[o++];
 	}
-	while (n > p2 && m < (p1 + p2))
+	while (n >= p2 && m < (p1 + p2))
 		x[m++] = s2[o++];
 	x[m] = '\0';
 	return (x);
