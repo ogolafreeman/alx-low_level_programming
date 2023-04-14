735bf0cb550616f372a9f23df4dce3c21eda3159
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-04-13 17:32:29 -0700

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,98 +2,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define ERR_MSG "Error"
-
 /**
- * is_digit - checks if a string contains a non-digit char
- * @s: string to be evaluated
- *
- * Return: 0 if a non-digit is found, 1 otherwise
- */
-int is_digit(char *s)
-{
-	int a = 0;
-
-	while (s[a])
-	{
-		if (s[a] < '0' || s[a] > '9')
-			return (0);
-		a++;
-	}
-	return (1);
-}
-/**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
- */
-int _strlen(char *s)
-{
-	int a = 0;
-
-	while (s[a] != '\0')
-	{
-		a++;
-	}
-	return (a);
-}
-/**
- * errors - handles all errors in main
- */
-void errors(void)
-{
-	printf("Error\n");
-	exit(98);
-}
-/**
- * main - multiple two positive numbers
- * @arg: number of arguments
- * @avg: arrays of arguments
- *
- * Return: always 0
+ * main - multiplies tqo numbers as arguments
+ * @arg: number of argumet
+ * @avg: the argument;
+ * Return: Always 0
  */
 int main(int arg, char *avg[])
 {
-	char *m1, *m2;
-	int p, p1, p2, a, cry, d1, d2, *rst, b = 0;
+	unsigned long typ;
+	int x, y;
 
-	m1 = avg[1], m2 = avg[2];
-
-	if (arg != 3 || !is_digit(m1) || !is_digit(m2))
-		errors();
-	p1 = _strlen(m1);
-	p2 = _strlen(m2);
-	p = p1 + p2 + 1;
-	rst = malloc(sizeof(int) * p);
-	if (!rst)
-		return (1);
-	for (a = 0; a <= p1 + p2; a++)
-		rst[a] = 0;
-	for (p1 = p1 - 1; p1 >= 0; p1--)
+	if (arg != 3)
 	{
-		d1 = m1[p1] - '0';
-		cry = 0;
-		for (p2 = _strlen(m2) - 1; p2 >= 0; p2--)
+		printf("Error\n");
+		exit(98);
+	}
+	for (x = 1; x < arg; x++)
+	{
+		for (y = 0; avg[x][y] != '\0'; y++)
 		{
-			d2 = m2[p2] - '0';
-			cry += rst[p1 + p2 + 1] + (d1 * d2);
-			rst[p1 + p2 + 1] = cry % 10;
-			cry /= 10;
+			if (avg[x][y] > 57 || avg[x][y] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
 		}
-		if (cry > 0)
-			rst[p1 + p2 + 1] += cry;
 	}
-	for (a = 0; a < p - 1; a++)
-	{
-		if (rst[a])
-			b = 1;
-		if (b)
-			_putchar(rst[a] + '0');
-	}
-	if (!b)
-		_putchar('0');
-	_putchar('\n');
-	free(rst);
+	typ = atoi(avg[1]) * atoi(avg[2]);
+	printf("%lu\n", typ);
 	return (0);
 }
