833da83bc76788adb4f1df8b0a7a273188b21e9c
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-04-18 05:24:44 -0700

    structure, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog's basic information
+ * @name: 1st Member
+ * @age: 2nd Member
+ * @owner: 3rd Member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+ * dog_type - typedef for struct  dog
+ */
+typedef struct dog dog_type;
+#endif
