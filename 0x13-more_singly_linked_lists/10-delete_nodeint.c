07ae436bb1f9e255bdaf9ee906c2f113132e3230
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-05-02 19:38:39 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,7 +16,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (*head == NULL)
 		return (-1);
 
-	if (idex == 0)
+	if (index == 0)
 	{
 		*head = (*head)->next;
 		free(ext);
