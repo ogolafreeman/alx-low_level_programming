3d81240070804c80d67a4ed367d4595cbb73bea4
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-05-08 14:05:14 -0700

    3. cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,16 +1,16 @@
 #include "main.h"
 #include <stdio.h>
-#include <errno.h>
+
 /**
- * file_error - checks if files can be opened.
+ * error_file - checks if files can be opened.
  * @file_from: file_from.
  * @file_to: file_to.
  * @argv: arguments vector.
  * Return: no return.
  */
-void file_error(int file_from, int file_to, char *argv[])
+void error_file(int file_from, int file_to, char *argv[])
 {
-	if (file_from == 1)
+	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
@@ -21,50 +21,52 @@ void file_error(int file_from, int file_to, char *argv[])
 		exit(99);
 	}
 }
+
 /**
- * main - check the code for file.
+ * main - check the code for Holberton School students.
  * @argc: number of arguments.
  * @argv: arguments vector.
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, close_error;
-	ssize_t xchars, nwr;
-	char buff[1024];
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
+	char buf[1024];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
+
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	file_error(file_from, file_to, argv);
+	error_file(file_from, file_to, argv);
 
-	xchars = 1024;
-	while (xchars == 1024)
+	nchars = 1024;
+	while (nchars == 1024)
 	{
-		xchars = read(file_from, buff, 1024);
-		if (xchars == -1)
-			file_error(-1, 0, argv);
-		nwr = write(file_to, buff, xchars);
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
 		if (nwr == -1)
-			file_error(0, -1, argv);
+			error_file(0, -1, argv);
 	}
 
-	close_error = close(file_from);
-	if (close_error == -1)
+	err_close = close(file_from);
+	if (err_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 
-	close_error = close(file_to);
-	if (close_error == -1)
+	err_close = close(file_to);
+	if (err_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	return (0);
-}
+
