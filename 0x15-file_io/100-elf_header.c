8f78a1f706f0546ddd37abd8a78aed4423645512
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-05-08 13:02:24 -0700

    4. elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -56,7 +56,7 @@ void print_magic(unsigned char *e_ident)
 	{
 		printf("%02x", e_ident[ind]);
 
-		if (index == EI_NIDENT - 1)
+		if (ind == EI_NIDENT - 1)
 			printf("\n");
 		else
 			printf(" ");
