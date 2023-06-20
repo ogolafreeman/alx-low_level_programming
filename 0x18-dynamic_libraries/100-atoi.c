95994ab7d22829fc0f0ac73821a852e0dead58dc
ogolafreeman <jacktoneokwemba14@gmail.com>
2023-06-19 17:14:01 -0700

    1. Without libraries what have we? We have no past and no future

--- 0x18-dynamic_libraries/100-atoi.c
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -1,10 +1,11 @@
+#include "main.h"
 /**
  * _atoi - Creates a file.
  * @s: A pointer to the name of the file to create
  * Return: ..
  */
 
-int _atoi(char *s)
+int _atoi(void)
 {
-
+	return (0);
 }
