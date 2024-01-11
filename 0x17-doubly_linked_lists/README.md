d97c8b8eec821068ca5d69eda84b78b9382f0749
Jacktone Okwemba <110520342+ogolafreeman@users.noreply.github.com>
2024-01-12 00:41:11 +0300

    deleted

--- 0x17-doubly_linked_lists/README.md
+++
@@ -1,3 +0,0 @@
-# 0x17. C - Doubly linked lists
-# Learning Objectives
-- At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -P /tmp/ https://github.com/dennisnderitu254/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libtest.so
-export LD_PRELOAD =/tmp/libtest.so
