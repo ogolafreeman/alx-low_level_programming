477d84cc45ce1d11027f785aefc27e5b8da0a7c0
Jacktone Okwemba <110520342+ogolafreeman@users.noreply.github.com>
2024-02-02 22:45:18 +0300

    0x1A. C - Hash tables

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0
+ */
+int check_num(char *str)
+{
+	unsigned int x;
+
+	x = 0;
+	while (x < strlen(str))
+	{
+		if (!isdigit(str[x]))
+		{
+			return (0);
+		}
+		x++;
+	}
+	return (1);
+}
+/**
+ * main - prints the name of the program
+ * @argc: count arguments
+ * @argv: arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+	int y;
+	int z = 0;
+
+	x = 1;
+	while (x < argc)
+	{
+		if (check_num(argv[x]))
+		{
+			y = atoi(argv[x]);
+			z += y;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		x++;
+	}
+	printf("%d\n", z);
+	return (0);
+}
